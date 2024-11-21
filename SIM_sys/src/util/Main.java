409333e15ef11b590f7c138dd5cf00cfd02cf355
moonpoison <moonpoison@kakao.com>
2024-11-22 03:29:12 +0900

    [Fix] refactoring

--- SIM_sys/src/util/Main.java
+++ SIM_sys/src/util/Main.java
@@ -6,8 +6,6 @@ import ui.LoginUI;
 
 public class Main {
 	public static void main(String[] args) {
-		DatabaseManager dm = DatabaseManager.getInstance();
-
 		LoginUI loginUI = new LoginUI();
 	}
 }
